cbfc84ebb6b5f129b71de5ebf44563a91c579f65
Marina Krasnoruzhskaya <krasnoruzhskayamarina@yandex.ru>
2024-09-30 11:50:53 +0300

    add model Habit

--- config/settings.py
+++ config/settings.py
@@ -43,11 +43,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
-    'rest_framework',
-    'phonenumber_field',
-    'rest_framework_simplejwt',
+    "rest_framework",
+    "phonenumber_field",
+    "rest_framework_simplejwt",
 
-    'users',
+    "users",
+    "habits"
 ]
 
 MIDDLEWARE = [
---
+++ habits/models.py
@@ -0,0 +1,83 @@
+from django.db import models
+
+from config.settings import AUTH_USER_MODEL
+
+
+NULLABLE = {"blank": True, "null": True}
+
+
+class Habit(models.Model):
+    """ Класс для модели Привычка """
+
+    PERIODS = {
+        ("1 time in 1 days", "1 раз в день"),
+        ("1 time in 2 days", "1 раз в 2 дня"),
+        ("1 time in 3 days", "1 раз в 3 дня"),
+        ("1 time in 4 days", "1 раз в 4 дня"),
+        ("1 time in 5 days", "1 раз в 5 дня"),
+        ("1 time in 6 days", "1 раз в 6 дня"),
+        ("1 time in 7 days", "1 раз в неделю"),
+    }
+
+    user = models.ForeignKey(
+        AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name="Пользователь",
+    )
+    place = models.CharField(
+        max_length=60,
+        verbose_name="Место выполнения",
+        help_text="Укажите место выполнения привычки",
+    )
+    time = models.TimeField(
+        verbose_name="Время выполнения",
+        help_text="Укажите время выполнения привычки",
+    )
+    action = models.CharField(
+        max_length=100,
+        verbose_name="Действие",
+        help_text="Укажите действие, которое нужно выполнять",
+    )
+    is_pleasant_habit = models.BooleanField(
+        verbose_name="Приятная привычка",
+        help_text="Укажите, является ли привычка приятной",
+        default=False,
+        **NULLABLE,
+    )
+    associated_habit = models.ForeignKey(
+        "Habit",
+        verbose_name="Связанная привычка",
+        help_text="Укажите связанную привычку",
+        on_delete=models.SET_NULL,
+        **NULLABLE,
+        related_name="habits",
+    )
+    periodicity = models.CharField(
+        choices=PERIODS,
+        max_length=30,
+        verbose_name="Периодичность выполнения",
+        help_text="Укажите периодичность выполнения привычки",
+    )
+    reward = models.CharField(
+        max_length=100,
+        verbose_name="Вознаграждение",
+        help_text="Укажите вознвграждение за выполнение привычки",
+        **NULLABLE,
+    )
+    time_to_complete = models.PositiveIntegerField(
+        verbose_name="Время на выполнение",
+        help_text="Укажите время на выполнение привычки",
+    )
+    is_publicity = models.BooleanField(
+        verbose_name="Публичная привычка",
+        help_text="Укажите, является ли привычка публичная",
+        default=False,
+        **NULLABLE,
+    )
+
+    def __str__(self):
+        return f"я буду {self.action} в {self.time} в {self.place}"
+
+    class Meta:
+        verbose_name = "Привычка"
+        verbose_name_plural = "Привычки"
