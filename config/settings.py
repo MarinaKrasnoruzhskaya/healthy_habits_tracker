a8ad93672bf7030798511ec3bf459ba27f255594
Marina Krasnoruzhskaya <krasnoruzhskayamarina@yandex.ru>
2024-10-03 00:00:35 +0300

    add celery

--- config/settings.py
+++ config/settings.py
@@ -153,3 +153,13 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
 }
+
+CELERY_TIMEZONE = TIME_ZONE
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
+
+# set the celery broker url
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
+
+# set the celery result backend
+CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
